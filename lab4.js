98d6a33bff5188c1dca525280e8cfb4f5e44ff2c
DanielCruzAr <daniel_cruzar@outlook.com>
2021-02-14 00:55:33 -0600

    lab 4

---
+++ lab4.js
@@ -0,0 +1,13 @@
+function uno(n){
+    for(let i = 1; i <= n; i++){
+        console.log(i*i);
+        console.log(i*i*i);
+    }
+}
+
+function dos(){
+    let x = Math.floor(Math.random()*100);
+    let x = Math.floor(Math.random()*100);
+}
+
+function tres(){}
