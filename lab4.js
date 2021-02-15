fcd937080a5912c153bc1e3fd6b0804567df4eda
DanielCruzAr <daniel_cruzar@outlook.com>
2021-02-14 23:55:20 -0600

    Add lab 4

--- lab4.js
+++ lab4.js
@@ -1,13 +1,75 @@
-function uno(n){
-    for(let i = 1; i <= n; i++){
-        console.log(i*i);
-        console.log(i*i*i);
-    }
+//Ejercicio1
+function uno(){
+	const n = window.prompt('número: ');
+
+	for(let i = 1; i <= n; i++){
+		document.write('<br><tr><td> ' + i + '</td><td> ' 
+        + i*i + '</td><td> ' + i*i*i + '</td></tr>');
+	}
 }
 
+// Ejercicio2
 function dos(){
-    let x = Math.floor(Math.random()*100);
-    let x = Math.floor(Math.random()*100);
+	let time = Date.now();
+	const x = Math.floor(Math.random() * 100);
+	const y = Math.floor(Math.random() * 100);
+
+    const res = window.prompt('Ingrese resultado: ' + x + " + " + y);
+	let end = Date.now();
+
+    if(res == (x+y)){
+        document.write('<p><strong>Correcto!</strong> Tardaste '+ ((end-time)/1000) + ' segundos</p>')
+    } else{
+        document.write('<p><strong>Incorrecto!</strong> Tardaste '+ ((end-time)/1000) + ' segundos</p>')
+	}
 }
 
-function tres(){}
+// Ejercicio3
+function contador(numArr){
+	let negativos = 0;
+	let ceros = 0;
+	let positivos = 0;
+	numArr.forEach( num => {
+		if (num < 0){negativos += 1}
+		else if (num == 0){ceros += 1}
+		else {positivos += 1}
+	});
+	return {'negativos':negativos, 'ceros':ceros, 'positivos':positivos}
+}
+
+function tres(){
+	const res = contador([-1, 0, 1, 2, 0, 10]);
+	document.write(JSON.stringify(res))
+}
+
+
+// Ejercicio4
+function promedios(arr2D){
+	const avg = [];
+	arr2D.forEach(arr => {
+		const prom = arr.reduce((a, b) => (a + b)) / arr.length;
+		avg.push(prom);
+	});
+	return avg;
+}
+
+function cuatro(){
+	const res = promedios([[1,2,3], [12,43,34], [120,24,398]]);
+	document.write(JSON.stringify(res));
+}
+
+
+// Ejercicio5
+function inverso(n){
+	const arrNum = n.toString().split("");
+	return parseInt(arrNum.reverse().join(""));
+}
+
+function cinco() {
+	const num = window.prompt("Ingrese un número: ")
+	const res = inverso(num);
+	document.write(res);
+}
+
+//Ejercicio6
+function seis(){}
